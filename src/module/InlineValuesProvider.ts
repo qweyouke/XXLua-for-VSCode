e7f3e2abfb001610b1eaf53e1ef66f3918a603cb
Xxiong <597272622@qq.com>
2022-01-10 11:28:57 +0800

    1、调试内联值显示优化 2、添加luaSocket库供无该库的项目使用

--- src/module/InlineValuesProvider.ts
+++ src/module/InlineValuesProvider.ts
@@ -15,10 +15,11 @@ interface LineRange {
     lineNum: number
     startIdx: number
     endIdx: number
+    varName: string
 }
 
 //查找本地
-function findVar(lineNum: number, str: string, allValues: Map<string, LineRange>, isEnd: boolean): boolean {
+function findVar(lineNum: number, str: string, uniqueValues: Map<string, LineRange>, nonUniqueValues: LineRange[], isEnd: boolean): boolean {
     if (!str.match(LOCAL_EXP5)) {
         str = str.replace(LOCAL_EXP4, "");
         
@@ -28,26 +29,37 @@ function findVar(lineNum: number, str: string, allValues: Map<string, LineRange>
                 for (const iterator of match) {
                     const varName = iterator[0];
                     if (!DebugUtil.getInstance().isFilterStr(varName) && !IS_NUMBER_REGEXP.test(varName) && !str.match(new RegExp(varName + LOCAL_EXP2))) {
-                        allValues.set(varName, {
+                        uniqueValues.set(varName, {
                             lineNum: lineNum,
                             startIdx: iterator.index ?? 0,
-                            endIdx: (iterator.index ?? 0) + varName.length
+                            endIdx: (iterator.index ?? 0) + varName.length,
+                            varName: varName
                         });
                     }
                 }
                 return true;
             }
         } else {
+            var isFindLocal = str.indexOf("local") !== -1 ? true : false;
+            var isFindFunction = str.indexOf("function") !== -1 ? true : false;
+            var equalIdx = str.indexOf("=");
+
             var match = str.matchAll(LOCAL_EXP1);
             if (match) {
                 for (const iterator of match) {
                     const varName = iterator[0];
                     if (!DebugUtil.getInstance().isFilterStr(varName) && !str.match(new RegExp(varName + LOCAL_EXP2)) && !str.match(new RegExp(LOCAL_EXP3 + varName))) {
-                        allValues.set(varName, {
+                        var value = {
                             lineNum: lineNum,
                             startIdx: iterator.index ?? 0,
-                            endIdx: (iterator.index ?? 0) + varName.length
-                        });
+                            endIdx: (iterator.index ?? 0) + varName.length,
+                            varName: varName
+                        };
+                        if (isFindFunction || (isFindLocal && equalIdx !== -1 && str.indexOf(varName) < equalIdx)) {
+                            nonUniqueValues.push(value);
+                        } else {
+                            uniqueValues.set(varName, value);
+                        }
                     }
                 }
                 return true;
@@ -59,14 +71,15 @@ function findVar(lineNum: number, str: string, allValues: Map<string, LineRange>
 }
 
 //查找参数
-function findParamVar(lineNum: number, str: string, allValues: Map<string, LineRange>, isEnd: boolean): boolean {
+function findParamVar(lineNum: number, str: string, uniqueValues: Map<string, LineRange>, nonUniqueValues: LineRange[], isEnd: boolean): boolean {
     const match = PARAM_EXP.exec(str);
     if (match) {
         const varName = match[0];
-        allValues.set(varName, {
+        nonUniqueValues.push({
             lineNum: lineNum,
             startIdx: match.index,
-            endIdx: match.index + varName.length
+            endIdx: match.index + varName.length,
+            varName: varName
         });
         return true;
     }
@@ -79,26 +92,34 @@ const handlerFuncList = [
 ];
 
 function provideInlineValues(document: vscode.TextDocument, viewPort: vscode.Range, context: vscode.InlineValueContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.InlineValue[]> {
-    const allValues = new Map<string, LineRange>();
+    //唯一值 以变量名为key
+    const uniqueValues = new Map<string, LineRange>();
+    //非唯一值
+    const nonUniqueValues: LineRange[] = [];
     const endLine = context.stoppedLocation.end.line;
     for (let l = viewPort.start.line; l <= endLine; l++) {
         const str = document.lineAt(l).text;
         for (const key in handlerFuncList) {
             const func = handlerFuncList[key];
-            if (func(l, str, allValues, l === endLine)) {
+            if (func(l, str, uniqueValues, nonUniqueValues, l === endLine)) {
                 break;
             }
         }
     }
 
     let ret: vscode.InlineValue[] = [];
-    for (const [varName, lineRange] of allValues) {
+    for (const [varName, lineRange] of uniqueValues) {
         const varRange = new vscode.Range(lineRange.lineNum, lineRange.startIdx, lineRange.lineNum, lineRange.endIdx);
         if (lineRange.lineNum === endLine) {
             ret.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
         } else {
             ret.push(new vscode.InlineValueVariableLookup(varRange, varName));
         }
+    }
+    for (const key in nonUniqueValues) {
+        const lineRange = nonUniqueValues[key];
+        const varRange = new vscode.Range(lineRange.lineNum, lineRange.startIdx, lineRange.lineNum, lineRange.endIdx);
+        ret.push(new vscode.InlineValueVariableLookup(varRange, lineRange.varName));
     }
 
     return ret;
